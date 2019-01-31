0795c7a61fbb60b62152825e76175d24218c5613
Adam Blake <theadamattack@gmail.com>
2019-01-31 10:28:34 -0800

    Update pre-exercise code
    Make the pre-exercise code match czi-stats-course sandboxes

--- learnosity-datacamp.js
+++ learnosity-datacamp.js
@@ -3,30 +3,37 @@ LearnosityAmd.define(["jquery-v1.10.2"], function ($) {
 	const getDataCampHTML = (extraPreExerciseCode, extraSampleCode) => `
 		<div data-datacamp-exercise data-lang="r" data-height="350" data-show-run-button="true" data-show-solution-before="false" data-no-lazy-load="true">
 			<code data-type="pre-exercise-code">
-				require(mosaic)
 				require(tidyverse)
-				require(ggformula)
-				require(supernova)
-				require(Lock5Data)
-				require(Lock5withR)
-				Fingers <- supernova::Fingers
-				Servers <- supernova::Servers
-				Survey <- supernova::Survey
-				TipExperiment <- supernova::TipExperiment
-				MindsetMatters <- Lock5Data::MindsetMatters
-				HappyPlanetIndex <- Lock5Data::HappyPlanetIndex
+	            require(readr)
+	            require(mosaic)
+	            require(Lock5withR)
+	            require(fivethirtyeight)
+	            require(okcupiddata)
+	            require(supernova)
+
+	            Fingers <- supernova::Fingers
+	            Servers <- supernova::Servers
+	            Survey <- supernova::Survey
+	            TipExperiment <- supernova::TipExperiment
+	            MindsetMatters <- Lock5Data::MindsetMatters
+	            HappyPlanetIndex <- Lock5Data::HappyPlanetIndex
+	            candy_rankings <- fivethirtyeight::candy_rankings %>%
+	                mutate(nutty = peanutyalmondy, sugarpercent = sugarpercent * 100) %>%
+	                select(competitorname, winpercent, chocolate, fruity, nutty, hard, bar, sugarpercent, pricepercent)
+	            selfies <- read_csv("https://docs.google.com/spreadsheets/d/1jqMg3-L4Z5bK5FCjCC2rv6Za4qzM5nq_Yn6k_rJmZ6M/export?format=csv")
 				${extraPreExerciseCode}
 			</code>
-			<code data-type="sample-code">    
+			<code data-type="sample-code">
 				${extraSampleCode}
 			</code>
-			<code data-type="solution">   
+			<code data-type="solution">
 			</code>
-			<code data-type="sct">  
+			<code data-type="sct">
 			</code>
+			<div data-type="hint">All code you have used in class should work here as well.</div>
 		</div>
 	`;
-	
+
 	// lock to make sure we don't start loading the script from multiple instances at the same time
 	let loadingDCLScript = false;
 
@@ -66,7 +73,7 @@ LearnosityAmd.define(["jquery-v1.10.2"], function ($) {
 					}
 				});
 		}
-		
+
 	}
 
 	return { Feature: DataCampFeature };
